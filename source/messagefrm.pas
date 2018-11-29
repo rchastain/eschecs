8c5e5b491540d4c108518f5f36b76f3cf7e8650a
fredvs <fiens@hotmail.com>
2018-11-29 20:57:27 +0100

    Added web site of Eschecs in About form.

--- source/messagefrm.pas
+++ source/messagefrm.pas
@@ -10,7 +10,7 @@ uses
   {$ENDIF}
   SysUtils, Classes, fpg_base, fpg_main, language,
   {%units 'Auto-generated GUI code'}
-  fpg_form, fpg_button, fpg_label
+  fpg_form, fpg_button, fpg_label, fpg_hyperlink
   {%endunits}
   ;
 
@@ -22,6 +22,7 @@ type
     Button1: TfpgButton;
     Label1: TfpgLabel;
     Label2: TfpgLabel;
+    Hyperlink1: TfpgHyperlink;
     {@VFD_HEAD_END: messagefrm}
     procedure AfterCreate; override;
     procedure closemsg(sender : Tobject);
@@ -32,7 +33,7 @@ type
 {$define read_interface}
 {$undef read_implementation}
 
- procedure ShowMessageFrm(AMessage1, AMessage2, ATitle, AButton : string);
+ procedure ShowMessageFrm(AMessage1, AMessage2, ATitle, AButton, AWebsite : string);
 
 {$I icon.inc} 
 
@@ -44,7 +45,7 @@ begin
 close;
 end;
 
-procedure ShowMessageFrm(AMessage1, AMessage2, ATitle, AButton : string);
+procedure ShowMessageFrm(AMessage1, AMessage2, ATitle, AButton, AWebsite : string);
 var
 mwidth: integer;
 msgfrm : Tmessagefrm;
@@ -55,6 +56,7 @@ begin
     msgfrm.Button1.text := Abutton;
     msgfrm.label1.text := AMessage1;
     msgfrm.label2.text := AMessage2;
+    
 
     msgfrm.WindowTitle := ATitle;
 
@@ -68,6 +70,16 @@ msgfrm.label1.top := 12;
 
 msgfrm.label1.left := (msgfrm.width - msgfrm.label1.width) div 2;
 msgfrm.label2.left := (msgfrm.width - msgfrm.label2.width) div 2;
+
+if AWebsite <> '' then
+begin
+msgfrm.Hyperlink1.text := Awebsite;
+msgfrm.Hyperlink1.left := (msgfrm.width - msgfrm.Hyperlink1.width) div 2;
+msgfrm.Hyperlink1.visible := true;
+msgfrm.Button1.top := msgfrm.Button1.top + 20;
+msgfrm.height := msgfrm.height +20;
+end;
+
 msgfrm.button1.left := (msgfrm.width - msgfrm.button1.width) div 2;
 
 msgfrm.ShowModal;
@@ -82,7 +94,7 @@ begin
   {%region 'Auto-generated GUI code' -fold}
   {@VFD_BODY_BEGIN: messagefrm}
   Name := 'messagefrm';
-  SetPosition(359, 541, 427, 84);
+  SetPosition(443, 310, 427, 84);
   WindowTitle := '';
   IconName := 'vfd.eschecs';
   BackGroundColor := $80000001;
@@ -95,25 +107,25 @@ begin
   with Button1 do
   begin
     Name := 'Button1';
-    SetPosition(176, 56, 80, 26);
+    SetPosition(168, 54, 83, 26);
     Text := 'Close';
     FontDesc := '#Label1';
     ImageName := '';
+    ModalResult := mrOK;
     ParentShowHint := False;
     TabOrder := 1;
-    ModalResult := mrOK;
-   end;
+  end;
 
   Label1 := TfpgLabel.Create(self);
   with Label1 do
   begin
     Name := 'Label1';
-    SetPosition(16, 12, 2, 15);
+    SetPosition(16, 12, 91, 15);
     Alignment := taCenter;
     AutoSize := True;
     FontDesc := '#Label1';
     ParentShowHint := False;
-    Text := '';
+    Text := 'ddddddddddddd';
     Hint := '';
   end;
 
@@ -121,12 +133,28 @@ begin
   with Label2 do
   begin
     Name := 'Label2';
-    SetPosition(16, 32, 2, 15);
+    SetPosition(16, 32, 105, 15);
     Alignment := taCenter;
     AutoSize := True;
     FontDesc := '#Label1';
     ParentShowHint := False;
-    Text := '';
+    Text := 'xxxxxxxxxxxxxxx';
+    Hint := '';
+  end;
+
+  Hyperlink1 := TfpgHyperlink.Create(self);
+  with Hyperlink1 do
+  begin
+    Name := 'Hyperlink1';
+    SetPosition(16, 50, 120, 15);
+    FontDesc := 'Liberation Sans-8:antialias=true:underline';
+    HotTrackColor := TfpgColor($B11DBC1D);
+    HotTrackFont := 'Liberation Sans-8:antialias=true:underline:bold';
+    ParentShowHint := False;
+    Text := 'Eschecs GitHub web site';
+    TextColor := TfpgColor($B1004001);
+    URL := 'https://github.com/rchastain/eschecs';
+    Visible := False;
     Hint := '';
   end;
 
