79c11c22fb97d280a90f900009e837970d2995bf
Roland Chastain <roland.chastain@gmx.fr>
2019-11-04 13:27:09 +0100

    Modify COMMANDLINE.PAS

--- source/build.cmd
+++ source/build.cmd
@@ -1,5 +1,3 @@
 @echo off
-set path=C:\lazarus\fpc\3.0.4\bin\x86_64-win64
-rem set path=C:\FPC\3.0.4\bin\i386-win32
+
 fpc -dDEBUG -dGDI @extrafpc.cfg eschecs.pas
-rem fpc -dGDI @extrafpc.cfg eschecs.pas
--- source/commandline.pas
+++ source/commandline.pas
@@ -3,6 +3,9 @@ unit CommandLine;
 
 interface
 
+uses
+  SysUtils;
+
 function HasOption(const AName: string; out AValue: integer): boolean; overload;
 function HasOption(const AName: string; out AValue: string): boolean; overload;
 function HasOption(const AName: string; out AValue: boolean): boolean; overload;
@@ -10,49 +13,52 @@ function HasOption(const AName: string; out AValue: boolean): boolean; overload;
 implementation
 
 uses
-  SysUtils, RegExpr;
+  RegExpr;
 
 const
   C1 = '[-/]';
   C2 = '[=: ]';
-  
-var
-  LExpr: TRegExpr;
 
 function HasOption(const AName: string; out AValue: integer): boolean;
+var
+  LExpr: TRegExpr;
 begin
-  LExpr.Expression := Concat(C1, AName, C2, '(\d+)');
+  LExpr := TRegExpr.Create(Concat(C1, AName, C2, '(\d+)'));
+  LExpr.ModifierI := TRUE;
   result := LExpr.Exec(CmdLine);
   if result then
     AValue := StrToInt(LExpr.Match[1])
   else
     AValue := 0;
+  LExpr.Free;
 end;
 
 function HasOption(const AName: string; out AValue: string): boolean;
+var
+  LExpr: TRegExpr;
 begin
-  LExpr.Expression := Concat(C1, AName, C2, '(\w+)');
+  LExpr := TRegExpr.Create(Concat(C1, AName, C2, '(\w+)'));
+  LExpr.ModifierI := TRUE;
   result := LExpr.Exec(CmdLine);
   if result then
     AValue := LExpr.Match[1]
   else
     AValue := '';
+  LExpr.Free;
 end;
 
 function HasOption(const AName: string; out AValue: boolean): boolean;
+var
+  LExpr: TRegExpr;
 begin
-  LExpr.Expression := Concat(C1, AName, C2, '(FALSE|TRUE)');
+  LExpr := TRegExpr.Create(Concat(C1, AName, C2, '(FALSE|TRUE)'));
+  LExpr.ModifierI := TRUE;
   result := LExpr.Exec(CmdLine);
   if result then
     AValue := UpperCase(LExpr.Match[1]) = 'TRUE'
   else
     AValue := FALSE;
+  LExpr.Free;
 end;
 
-initialization
-  LExpr := TRegExpr.Create;
-  LExpr.ModifierI := TRUE;
-
-finalization
-  LExpr.Free;
 end.
